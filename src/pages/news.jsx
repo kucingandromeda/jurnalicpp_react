eb3e8f7773978ac2fcb5f7fef40c4884316ad089
kucingandromeda <agvayazem@gmail.com>
2024-02-26 07:41:06 +0700

    update agar mirip dengan branch router berhasil

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -1,3 +1,13 @@
+import { HeaderNavNews } from "./compunent/e_newsLook/a_header/headerNav";
+import { NewsArea } from "./compunent/e_newsLook/b_newsArea/newsArea";
+import { Footer } from "./compunent/e_newsLook/c_footer/footer";
+
 export const NewsShowing = () => {
-  return <div className="hehehe">halo ini pages news</div>;
+  return (
+    <>
+      <HeaderNavNews></HeaderNavNews>
+      <NewsArea></NewsArea>
+      <Footer></Footer>
+    </>
+  );
 };
