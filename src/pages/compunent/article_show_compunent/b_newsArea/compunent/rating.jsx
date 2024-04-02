58f9cba88bdd93f707edf8a5a9654d58c8c1e137
kucingandromeda <agvayazem@gmail.com>
2024-04-02 09:38:09 +0700

    membersihkan rating.jsx

--- src/pages/compunent/article_show_compunent/b_newsArea/compunent/rating.jsx
+++ src/pages/compunent/article_show_compunent/b_newsArea/compunent/rating.jsx
@@ -16,59 +16,9 @@ export const Rating = ({
   setRating,
   setRateHidden,
 }) => {
-  const [dataApi, setDataApi] = useState(null);
-  // const [dataFromDb, setDataFromDb] = useState(null);
   const [rateLoad, setRateLoad] = useState(false);
-  // const [rateHidden, setRateHidden] = useState(false);
-  // const [rating, setRating] = useState(0);
-  // const [staticRate, setStaticRate] = useState(0);
-
-  const [nama_akun, setNama_akun] = useState("");
 
   const navi = useNavigate();
-
-  useEffect(() => {
-    // fetch(`${import.meta.env.VITE_API_URL_NEWSDATA}${window.location.pathname}`)
-    //   .then((res) => res.json())
-    //   .then((resJSON) => {
-    //     setDataFromDb(resJSON);
-    //     fetch(import.meta.env.VITE_API_SESSION, { credentials: "include" })
-    //       .then((res) => res.json())
-    //       .then((res) => {
-    //         if (!res.servertext) return;
-    //         setNama_akun(res.nama_akun);
-
-    //         const form = new FormData();
-    //         form.append("user", res.nama_akun);
-    //         form.append("judul", resJSON[0].judul);
-
-    //         fetch(import.meta.env.VITE_API_CHECK_RATE, {
-    //           method: "POST",
-    //           credentials: "include",
-    //           body: form,
-    //         })
-    //           .then((res) => res.json())
-    //           .then((res) => {
-    //             if (!res.stat) return;
-    //             setStaticRate(res.value);
-    //             setRating(res.value);
-    //             setRateHidden(true);
-    //           });
-    //       });
-    //   });
-
-    if (apiData) {
-      setDataApi(apiData);
-    } else {
-      fetch(
-        `${import.meta.env.VITE_API_URL_SHOW_NEWS}${window.location.pathname}`
-      )
-        .then((res) => res.text())
-        .then((res) => {
-          setDataApi(res);
-        });
-    }
-  }, []);
 
   const rateFn = () => {
     setRateLoad(true);
