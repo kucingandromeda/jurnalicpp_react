d2afe6e777617e4ed0f0716fa177e28a044a48e6
kucingandromeda <agvayazem@gmail.com>
2024-04-03 09:41:53 +0700

    saving aja

--- src/pages/compunent/ads_compunent/compunent/nativeAds.jsx
+++ src/pages/compunent/ads_compunent/compunent/nativeAds.jsx
@@ -3,18 +3,21 @@ import { useEffect, useRef } from "react";
 export const NativeAds = () => {
   const target = useRef(null);
   useEffect(() => {
-    const script = document.createElement("script");
-    // script.async = "async";
-    script.setAttribute("async", "async");
-    script.setAttribute("data-cfasync", false);
-    script.src =
-      "//pl22944050.profitablegatecpm.com/39158969dbde6d27fa3d4876361aeb23/invoke.js";
+    if (target.current && !target.current.firstChild) {
+      const script = document.createElement("script");
+      // script.async = "async";
+      script.setAttribute("async", "async");
+      script.setAttribute("data-cfasync", false);
+      script.src =
+        "//pl22944050.profitablegatecpm.com/39158969dbde6d27fa3d4876361aeb23/invoke.js";
 
-    const div = document.createElement("div");
-    div.id = "container-39158969dbde6d27fa3d4876361aeb23";
+      const div = document.createElement("div");
+      div.id = "container-39158969dbde6d27fa3d4876361aeb23";
 
-    target.current.append(script);
-    target.current.append(div);
+      target.current.append(script);
+      target.current.append(div);
+      console.log("running");
+    }
   }, []);
 
   return (
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,8 +5,27 @@ import { ArticleContainer } from "./compunent/article_container_compunent/Articl
 import { AdsArea } from "./compunent/ads_compunent/Ads_area";
 import { Powered } from "./compunent/powered_compunent/Powered";
 import { Footer } from "./compunent/footer_compunent/Footer";
+import { useRef, useEffect } from "react";
 
 export const Home = ({ urlFn, sectionFn }) => {
+  // const target = useRef(null);
+  // useEffect(() => {
+  //   if (target.current && !target.current.firstChild) {
+  //     const script = document.createElement("script");
+  //     // script.async = "async";
+  //     script.setAttribute("async", "async");
+  //     script.setAttribute("data-cfasync", false);
+  //     script.src =
+  //       "//pl22944050.profitablegatecpm.com/39158969dbde6d27fa3d4876361aeb23/invoke.js";
+
+  //     const div = document.createElement("div");
+  //     div.id = "container-39158969dbde6d27fa3d4876361aeb23";
+
+  //     target.current.append(script);
+  //     target.current.append(div);
+  //     console.log("running");
+  //   }
+  // }, [target]);
   return (
     <>
       <Navbar sectionFn={sectionFn}></Navbar>
@@ -17,7 +36,11 @@ export const Home = ({ urlFn, sectionFn }) => {
 
       <ArticleContainer urlFn={urlFn}></ArticleContainer>
 
+      {/* <div ref={target}></div> */}
+
       <AdsArea></AdsArea>
+
+      {/* <AdsArea></AdsArea> */}
 
       <Powered></Powered>
 
